a62cfb50a022ce149f80ca2196c54f41f5c932d6
Uliana Bobrova <uliana.bobrova@gmail.com>
2020-05-15 17:28:51 +0300

    Complete addSentence function

--- src/modules/addSentence.js
+++ src/modules/addSentence.js
@@ -4,16 +4,23 @@
 const addSentence = () => {
     const addBtn = document.querySelector('#show');
     const removeBtn = document.querySelector('#close');
-    const blockHidden = document.querySelector('.hidden');
+    const blockHidden = document.querySelectorAll('.hidden');
     const blockVisible = document.querySelector('.visible-sm-block');  
 
     addBtn.addEventListener('click', () => {
-        blockHidden.classList.toggle('hidden');
-        blockVisible.classList.toggle('visible-sm-block');
-        addBtn.style.display = 'none';
-        // addBtn.textContent = 'Скрыть..';
+        
+        blockHidden.forEach((elem) => {
+            elem.classList.toggle('hidden');
         });
-};
+        blockVisible.classList.toggle('visible-sm-block');
 
+        if(addBtn.innerHTML === 'Больше...'){
+            console.log('Hello');
+            addBtn.innerHTML = 'Скрыть..';
+            return;
+        } else {
+            addBtn.innerHTML = 'Больше...';}
+    });
+};
 
 export default addSentence;
